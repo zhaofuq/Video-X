41e0a6a6237e9c527e48105f8788f4c90b397523
huangkunzhe.hkz <huangkunzhe.hkz@alibaba-inc.com>
2025-06-04 20:34:11 +0800

    Fix merge lora

--- examples/wan2.1_fun/predict_t2v.py
+++ examples/wan2.1_fun/predict_t2v.py
@@ -253,7 +253,7 @@ if cfg_skip_ratio is not None:
 generator = torch.Generator(device=device).manual_seed(seed)
 
 if lora_path is not None:
-    pipeline = merge_lora(pipeline, lora_path, lora_weight)
+    pipeline = merge_lora(pipeline, lora_path, lora_weight, device=device)
 
 with torch.no_grad():
     video_length = int((video_length - 1) // vae.config.temporal_compression_ratio * vae.config.temporal_compression_ratio) + 1 if video_length != 1 else 1
@@ -293,7 +293,7 @@ with torch.no_grad():
         ).videos
 
 if lora_path is not None:
-    pipeline = unmerge_lora(pipeline, lora_path, lora_weight)
+    pipeline = unmerge_lora(pipeline, lora_path, lora_weight, device=device)
 
 def save_results():
     if not os.path.exists(save_path):
